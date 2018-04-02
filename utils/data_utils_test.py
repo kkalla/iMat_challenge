cd0b8ccced274b2dc1aaf288e579bc2205cc9d6d
Kkalla <dkswns333@gmail.com>
2018-04-02 19:53:15 +0900

    Add utils
    -add data_utils.py
    -download_data can download competition files(csv and json, not images)

---
+++ utils/data_utils_test.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  2 17:33:54 2018
+
+@author: user
+"""
+
+from data_utils import Data_loader
+
+def test_data_loader():
+    data_loader = Data_loader()
+    data_loader.load_datasets(data_dir='../data/')
+    
+def main():
+    test_data_loader()
+    
+if __name__ == '__main__':
+    main()
