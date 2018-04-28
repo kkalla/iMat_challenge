627fe4410da2493fd7309fa3a4eace2921b043b7
Kkalla <dkswns333@gmail.com>
2018-04-28 14:41:34 +0900

    Modify my_vgg19 model
    - weigths: 'imagenet' -> None

--- common/my_vgg19.py
+++ common/my_vgg19.py
@@ -8,7 +8,7 @@ Created on Sat Apr 28 14:04:03 2018
 from keras.applications.vgg19 import VGG19
 
 def load_model():
-    base_model = VGG19(include_top=True,weights='imagenet',classes=128)
+    base_model = VGG19(include_top=True,weights=None,classes=128)
     model = base_model
     
     return model
