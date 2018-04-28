72d448f9a8b20b211ea9bf44cef65ce369c87f90
Kkalla <dkswns333@gmail.com>
2018-04-28 14:50:34 +0900

    Modify my_vgg19 model
    -Add some layers into top of the model

--- common/my_vgg19.py
+++ common/my_vgg19.py
@@ -6,10 +6,21 @@ Created on Sat Apr 28 14:04:03 2018
 """
 
 from keras.applications.vgg19 import VGG19
+from keras.model import Model
+from keras.layers import Flatten,Dense,Dropout
+from keras.layers.normalization import BatchNormalization
+
+num_classes=128
 
 def load_model():
-    base_model = VGG19(include_top=True,weights=None,classes=128)
-    model = base_model
+    base_model = VGG19(include_top=False,weights='imagenet',input_shape=(224,224,3))
+    x = Flatten()(base_model.output)
+    x = Dense(4096, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    x = BatchNormalization()(x)
+    predictions = Dense(num_classes, activation='softmax')(x)
+    
+    model = Model(input = base_model.input,output=predictions)
     
     return model
 
